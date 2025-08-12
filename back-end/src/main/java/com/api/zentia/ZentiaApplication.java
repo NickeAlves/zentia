354160fc7e6bef7aeb5261f01f339dedc9de6f05
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-08-12 18:29:26 +0200

    :sparkles: init project with backend files
    Signed-off-by: Nicolas Alves <nicke.rodrigues1125@gmail.com>

---
+++ back-end/src/main/java/com/api/zentia/ZentiaApplication.java
@@ -0,0 +1,13 @@
+package com.api.zentia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZentiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZentiaApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/api/zentia/ZentiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.zentia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZentiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
