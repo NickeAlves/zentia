1c0acc63b9fdf4cf62df7cec55006ec38c1a44bf
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-08-16 20:16:11 +0200

    :sparkles: feat: upload front-end initials files with navbar
    Signed-off-by: Nicolas Alves <nicke.rodrigues1125@gmail.com>

---
+++ front-end/src/app/components/navbar/navbar.ts
@@ -0,0 +1,34 @@
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+  isMobileMenuOpen = false;
+
+  constructor(private router: Router) {}
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen = false;
+  }
+
+  navigateToLogin(): void {
+    this.router.navigate(['/auth/login']);
+    this.closeMobileMenu();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    if (event.target.innerWidth > 767.98) {
+      this.isMobileMenuOpen = false;
+    }
+  }
+}
